46c6cd6e056811e45110d290b863400bde258e0d
Jordi-D <devjordi.jd97@gmail.com>
2025-11-02 12:26:25 +0100

    Update pom.xml to upgrade springdoc-openapi to version 2.7.0

---
+++ src/main/java/com/proyecto/tfg/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.proyecto.tfg.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TFG Eventos API")
+                        .version("1.0")
+                        .description("API REST para gestionar eventos"));
+    }
+}
